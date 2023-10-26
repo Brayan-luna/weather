4aad21ec7e8acfecc765d5126a3ac276303b3abf
Brayan-luna <brayanjoselunapadilla@gmail.com>
2023-10-26 13:24:59 -0400

    add styless with tailwind and partOne

--- src/components/HomeWheter/Home.jsx
+++ src/components/HomeWheter/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import PartOne from "./partOne";
+import PartOne from "./PartOne/partOne";
 import './Home.css'
 function Home(){
     return(
-        <section className="sectionHome">
-            <PartOne/>
+        <section className="sectionHome ">
+            <div className="dark:bg-neutral-900 dark:text-white divContainer"> 
+                <PartOne/>
+            </div>
+            
         </section>
     )
 }
